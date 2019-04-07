da3a8554ea5a00173e53d4ec42a6663fe96f6950
Roman Mykhailiuk <mikhailiuk.roman@gmail.com>
2019-04-08 00:18:06 +0300

    changr

--- js/averege.js
+++ js/averege.js
@@ -4,7 +4,7 @@ for (var i = 0; i < arr.length; i++) {
 	total+=arr[i];
 	
 }
-let result = total/arr.length
+let result = (total/arr.length).toFixed(2);
 	return result;
 }
 average([1,4,2]) // 2.33
--- js/print.js
+++ js/print.js
@@ -3,6 +3,6 @@ let string = str;
 setTimeout(function(){
 	console.log(string);
 },n*1000);
-	return printTimeout;
+	
 }
 printTimeout('hello', 1) // hello will appear after 10secs in console
